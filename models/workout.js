346e08d9594726ad121589027cb55efbe15449ae
stevepae <stevepaemusic@gmail.com>
2021-04-10 23:42:52 -0400

    backend has been updated to allow for better functionality when creating form

--- models/workout.js
+++ models/workout.js
@@ -5,40 +5,40 @@ const workoutSchema = new Schema({
     uid: String,
     workout: {
         type: String, 
-        required: true},
+    },
     exercise: {
         type: String, 
-        required: true},
+        },
     reps1: {
         type: String, 
-        required: true},
+        },
     reps2: {
         type: String, 
-        required: true},
+        },
     reps3: {
         type: String, 
-        required: true},
+        },
     reps4: {
         type: String, 
-        required: true},
+        },
     reps5: {
         type: String, 
-        required: true},
+        },
     weight1: {
         type: String, 
-        required: true},
+        },
     weight2: {
         type: String, 
-        required: true},
+        },
     weight3: {
         type: String, 
-        required: true},
+        },
     weight4: {
         type: String, 
-        required: true},
+        },
     weight5: {
         type: String, 
-        required: true},
+        },
     }, {
     timestamps: true,
 });
