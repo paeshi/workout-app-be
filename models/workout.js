0321a75276f2f54ada01f6f2c443815a81bf3aea
stevepae <stevepaemusic@gmail.com>
2021-04-15 11:06:25 -0400

    updated model to include option to add notes

--- models/workout.js
+++ models/workout.js
@@ -1,47 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const workoutSchema = new Schema(
+  {
     uid: String,
     workout: {
-        type: String, 
+      type: String,
+      required: false,
     },
     exercise: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     reps1: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     reps2: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     reps3: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     reps4: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     reps5: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     weight1: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     weight2: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     weight3: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     weight4: {
-        type: String, 
-        },
+      type: String,
+      required: false,
+    },
     weight5: {
-        type: String, 
-        },
-    }, {
+      type: String,
+      required: false,
+    },
+    notes: {
+      type: String,
+      required: false,
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-
-module.exports = mongoose.model('Workout', workoutSchema)
+module.exports = mongoose.model("Workout", workoutSchema);
